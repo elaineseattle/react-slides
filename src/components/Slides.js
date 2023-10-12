eab0771c4cff839154ac8b56d59a2c7955df3a84
elaineseattle <84435178+elaineseattle@users.noreply.github.com>
2023-10-12 13:55:22 -0700

    Update Slides.js

--- src/components/Slides.js
+++ src/components/Slides.js
@@ -1,34 +1,65 @@
 import React, { useState } from 'react';
 
-function Slides({slides}) {
-    const [page, setPage] = useState(0);
+function Slides({ slides }) {
+    // Initialize state to keep track of the currently displayed slide index
+    const [currentSlide, setCurrentSlide] = useState(0);
 
-    const restartPage = () => {
-        setPage(0);
-    }
+    // Function to go to the next slide
+    const nextSlide = () => {
+        if (currentSlide < slides.length - 1) {
+            setCurrentSlide(currentSlide + 1);
+        }
+    };
 
-    const goPrevPage = () => {
-        setPage(page-1);
-    }
+    // Function to go to the previous slide
+    const prevSlide = () => {
+        if (currentSlide > 0) {
+            setCurrentSlide(currentSlide - 1);
+        }
+    };
 
-    const goNextPage = () => {
-        setPage(page+1);
-    }
+    // Function to restart the slides from the beginning
+    const restartSlides = () => {
+        setCurrentSlide(0);
+    };
+
+    // Retrieve the currently displayed slide from the slides array
+    const currentSlideData = slides[currentSlide];
 
     return (
         <div>
             <div id="navigation" className="text-center">
-                <button onClick={restartPage} disabled={page === 0} data-testid="button-restart" className="small outlined">Restart</button>
-                <button onClick={goPrevPage} disabled={page === 0} data-testid="button-prev" className="small">Prev</button>
-                <button onClick={goNextPage} disabled={page === slides.length-1} data-testid="button-next" className="small">Next</button>
+                <button
+                    data-testid="button-restart"
+                    className="small outlined"
+                    onClick={restartSlides}
+                    disabled={currentSlide === 0}
+                >
+                    Restart
+                </button>
+                <button
+                    data-testid="button-prev"
+                    className="small"
+                    onClick={prevSlide}
+                    disabled={currentSlide === 0}
+                >
+                    Prev
+                </button>
+                <button
+                    data-testid="button-next"
+                    className="small"
+                    onClick={nextSlide}
+                    disabled={currentSlide === slides.length - 1}
+                >
+                    Next
+                </button>
             </div>
             <div id="slide" className="card text-center">
-                <h1 data-testid="title">{slides[page].title}</h1>
-                <p data-testid="text">{slides[page].text}</p>
+                <h1 data-testid="title">{currentSlideData.title}</h1>
+                <p data-testid="text">{currentSlideData.text}</p>
             </div>
         </div>
     );
-
 }
 
 export default Slides;
